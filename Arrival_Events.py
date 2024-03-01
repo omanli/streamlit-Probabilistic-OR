2f21b7b665573556360b50c145a810e65e94ecc8
Murat Anlı <52565584+omanli@users.noreply.github.com>
2024-03-01 17:00:58 +0300

    Create Arrival_Events.py

---
+++ Arrival_Events.py
@@ -0,0 +1,15 @@
+"""
+Arrival Process Dynamics
+Emergence of Exponential Interarrival Times
+"""
+
+"""
+rs = 123
+N, n = 20, 100
+rng = np.random.RandomState(rs)
+A = np.cumsum(rng.uniform(0, 1, size=(N,n)), axis=1)
+X = rng.multivariate_normal(mean=[0, 0], cov=[[3, 3], [3, 4]], size=500)
+
+plt.stairs
+"""
+
