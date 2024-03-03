de8fbd6eacd1773bedd2b7475e5076482cf177e8
Murat Anlı <52565584+omanli@users.noreply.github.com>
2024-03-03 22:10:15 +0300

    Update Arrival_Events.py

--- Arrival_Events.py
+++ Arrival_Events.py
@@ -9,56 +9,112 @@ import numpy as np
 import pandas as pd
 
 st.set_page_config(page_title="Arrival Events")
-
-def dmy():
-    rng = np.random.RandomState(rs)
-    A = np.cumsum(rng.uniform(0, 1, size=(N,n)), axis=1)
-    E = np.random.multivariate_normal(mean=[0, 0], cov=[[3, 3], [3, 4]], size=500)
-    X = rng.multivariate_normal(mean=[0, 0], cov=[[3, 3], [3, 4]], size=500)
-    plt.stairs
-
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        chart.add_rows(new_rows)
-        last_rows = new_rows
-
-
 st.markdown("# Arrival Events")
 st.sidebar.header("Arrival Events")
-st.write("This demo tool simulates arrival events.")
+st.write("This demo tool simulates arrival events from multiple renewal processes.")
 st.write("Enjoy!")
 st.write("")
 
-rs = 123
 
-N = 3
-n = 10
-Rt = 1/4
+distr_par = {
+    None : '--',
+    'Exponential' : [1, 'Exponential(avg)'], 
+    'Uniform'     : [2, 'Uniform(a,b)'], 
+    'Triangular'  : [3, 'Triangular(a,m,b)'],
+}
 
-src  = [f"A{i+1:02}" for i in range(N)]
 
-A = np.cumsum(np.random.exponential(1/Rt, size=(N,n)), axis=1)
-E = [(A[i,k], src[i]) for i in range(N) for k in range(n)]
-E.sort()
+@st.cache_data
+def generate_event_times(rs, D, par, N, n):
+    if D is None:
+        return None, None
+    if D not in distr_par:
+        raise ValueError(f"Incorrect distribution selection={D!r}")
+
+    rng = np.random.RandomState(rs)
+    par = par.replace(',', ' ')
+    param = [float(p) for p in par.split()]
+    if len(param) != distr_par[distribution][0]:
+        raise ValueError(f"Incorrect parameter values={param!r} for distribution")
+
+    I = getattr(rng, D.lower())(*param, size=(N,n))
+    
+    A = np.cumsum(I, axis=1)
+
+    E = sorted([(A[i,k], src[i]) for i in range(N) for k in range(n)])
+
+    T = float(round(E[-1][0], 1))
+
+    return E, T
+
+
+
+
+cols = st.columns([1,1,1])
+with cols[0]:
+    random_seed = st.number_input("random seed:", value=123)
+    distribution = st.selectbox(
+        label='Interarrival Time Distribution:',
+        options=('Exponential', 'Uniform', 'Triangular'),
+        index=None,
+        placeholder='Choose a Distribution')
+with cols[1]:
+    num_sources  = st.number_input("number of sources:", value=3)
+    param_str = st.text_input(distr_par[distribution][1], value='')
+with cols[2]:
+    num_events  = st.number_input("number of arrivals:", value=10)
+with cols[0]:
+    st.write(f'You selected: {distribution}({param_str})')
+
+
+src  = [f"A{i+1:02}" for i in range(num_sources)]
+
+
+try:
+    E,T = generate_event_times(
+            random_seed,
+            distribution,
+            param_str,
+            num_sources,
+            num_events)
+except ValueError as e:
+    # e.message
+    st.error(getattr(e, 'message', repr(e)))
+
+
+t_range = st.slider(label='Plot Time Window', 
+                    min_value=0,
+                    max_value=int(T),
+                    step=1,
+                    value=(int(0.3*T), int(0.7*T)))
+
 
 data = []
-data.append(pd.DataFrame(dict(t=[e[0] for e in E], e=[e[1] for e in E])))
-for i in range(N):
-    d = dict(t=[e[0] for e in E if e[1]==src[i]], e=["A" for e in E if e[1]==src[i]])
+for i in range(num_sources + 1):
+    if i == 0:
+        d = dict(t=[e[0] for e in E], e=[e[1] for e in E])
+    else:
+        d = dict(t=[e[0] for e in E if e[1]==src[i-1]], e=["A" for e in E if e[1]==src[i-1]])
     data.append(pd.DataFrame(d))
+    idx = (data[-1].t >= t_range[0]) & (data[-1].t <= t_range[1])
+    data[-1] = data[-1].loc[idx]
+
 
 charts = []
 charts.append(alt.Chart(data[0])
                  .mark_point()
-                 .encode(x="t:Q", y="e:N"))
-for i in range(N+1):
+                 .encode(x=alt.X("t:Q", scale=alt.Scale(domain=[t_range[0],t_range[1]])),
+                         y=alt.X("e:N")))
+for i in range(num_sources+1):
     charts.append(alt.Chart(data[i])
                  .mark_point()
-                 .encode(x="t:Q", y="e:N"))
+                 .encode(x=alt.X("t:Q", scale=alt.Scale(domain=[t_range[0],t_range[1]])),
+                         y=alt.X("e:N")))
 LC = alt.layer(*charts)
 
 st.altair_chart(LC, use_container_width=True)
 
-st.dataframe(data[0])
+# st.dataframe(data[0])
+# st.write(dict(t=[e[0] for e in E if e[1]==src[-1]], e=["A" for e in E if e[1]==src[-1]]))
 
 st.button("Re-run")
